ce863706f41b15486586ca0c4d66b6b8c5b21ec6
Valentin Caloean <vcaloean@gmail.com>
2020-12-19 21:15:59 -0500

    Init commit

---
+++ src/test/java/org/caloean/DataStructures/DataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.caloean.DataStructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
